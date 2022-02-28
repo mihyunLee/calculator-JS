a5201fb640cfaace8eab7d2b06cdf8f669f052c3
mihyun <mihy621@gmail.com>
2022-02-28 23:43:30 +0900

    step 1 - 콘솔에 값을 출력하는 계산기

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,22 @@
-function App() {}
+function App() {
+  const calcurator = {
+    plus: function (a, b) {
+      console.log(a + b);
+    },
+    minus: function (a, b) {
+      console.log(a - b);
+    },
+    multi: function (a, b) {
+      console.log(a * b);
+    },
+    divide: function (a, b) {
+      console.log(a / b);
+    },
+  };
+  calcurator.plus(1, 2);
+  calcurator.minus(3, 5);
+  calcurator.multi(6, 2);
+  calcurator.divide(20, 3);
+}
 
 App();
