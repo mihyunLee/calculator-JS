30e24a16463b27ac006e1168028d9ca2c4f8b4c7
mihyun <mihy621@gmail.com>
2022-03-08 10:32:39 +0900

    feat: step 2 - input으로 사용자 입력을 받아 이벤트를 처리하기

--- src/js/app.js
+++ src/js/app.js
@@ -1,14 +1,30 @@
+const $ = (selector) => document.querySelector(selector);
+
 function App() {
-  const calculator = {
-    plus: (a, b) => console.log(a + b),
-    minus: (a, b) => console.log(a - b),
-    multi: (a, b) => console.log(a * b),
-    divide: (a, b) => console.log(a / b),
+  const calculateNumber = (e) => {
+    const firstNumber = Number($("#firstInput").value);
+    const secondNumber = Number($("#secondInput").value);
+    const op = e.target.innerText;
+    switch (op) {
+      case "+":
+        return firstNumber + secondNumber;
+      case "-":
+        return firstNumber - secondNumber;
+      case "x":
+        return firstNumber * secondNumber;
+      case "/":
+        return firstNumber / secondNumber;
+      default:
+        return "연산자를 클릭해주세요.";
+    }
   };
-  calculator.plus(1, 2);
-  calculator.minus(3, 5);
-  calculator.multi(6, 2);
-  calculator.divide(20, 3);
+
+  const showResult = (e) => {
+    $("#operation-result").innerText = calculateNumber(e);
+  };
+
+  $("#calculate-form").addEventListener("submit", (e) => e.preventDefault());
+  $("#operation-button").addEventListener("click", showResult);
 }
 
 App();
