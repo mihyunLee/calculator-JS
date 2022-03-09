98327b5c9d9a034c94fb68b479b55ed43b98065f
mihyun <mihy621@gmail.com>
2022-03-09 18:39:27 +0900

    refactor: step 2 - 하나의 input으로 사용자 입력 받아 이벤트 처리하기

--- src/js/app.js
+++ src/js/app.js
@@ -1,30 +1,55 @@
 const $ = (selector) => document.querySelector(selector);
 
 function App() {
-  const calculateNumber = (e) => {
-    const firstNumber = Number($("#firstInput").value);
-    const secondNumber = Number($("#secondInput").value);
-    const op = e.target.innerText;
-    switch (op) {
-      case "+":
-        return firstNumber + secondNumber;
-      case "-":
-        return firstNumber - secondNumber;
-      case "x":
-        return firstNumber * secondNumber;
-      case "/":
-        return firstNumber / secondNumber;
-      default:
-        return "연산자를 클릭해주세요.";
+  // Variables
+  let numOne = "",
+    numTwo = "",
+    ops = "",
+    result = "";
+
+  // +, -, x, / 을 클릭했을 때 변수에 값을 세팅한다.
+  const setValue = (e) => {
+    const isOpsButton = e.target.classList.contains("operation");
+    if (isOpsButton) {
+      if (result) {
+        numOne = "";
+        numTwo = "";
+        ops = "";
+      }
+      if (!numOne) {
+        numOne = Number($("#input-number").value);
+      }
+      ops = e.target.dataset.ops;
     }
   };
 
-  const showResult = (e) => {
-    $("#operation-result").innerText = calculateNumber(e);
+  // = 을 클릭했을 때 input에 결과값을 출력한다.
+  const displayResult = () => {
+    if (!numTwo && ops) {
+      numTwo = Number($("#input-number").value);
+      switch (ops) {
+        case "plus":
+          result = numOne + numTwo;
+          break;
+        case "minus":
+          result = numOne - numTwo;
+          break;
+        case "multi":
+          result = numOne * numTwo;
+          break;
+        case "divide":
+          result = numOne / numTwo;
+          break;
+      }
+      $("#input-number").value = result;
+    } else {
+      result = $("#input-number").value;
+    }
   };
 
   $("#calculate-form").addEventListener("submit", (e) => e.preventDefault());
-  $("#operation-button").addEventListener("click", showResult);
+  $("#operation-button").addEventListener("click", (e) => setValue(e));
+  $("#equal-button").addEventListener("click", displayResult);
 }
 
 App();
